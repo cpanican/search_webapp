96a04744fcc9a91d4dc87823c2c40c2153b6dead
Chris Panican <chrispanican@gmail.com>
2017-07-25 17:06:52 -0400

    Did some code cleanup

--- app.py
+++ app.py
@@ -28,7 +28,6 @@ def search():
 @webapp.route('/', methods=['POST'])
 def search_page():
     if request.method == 'POST':
-        # Initialize variables and inputs
         # Input form requests
         ndc_in = request.form['ndc']
         atc_in = request.form['atc']
@@ -38,16 +37,6 @@ def search_page():
         ndc_code = []
         atc_code = []
         umls_code = []
-
-        # Names of codes
-        ndc_label = []
-        atc_label = []
-        umls_label = []
-
-        # Tuple (name, code) of result
-        # ndc_res = []
-        # atc_res = []
-        # umls_res = []
 
         if ndc_in:
             ndc_code.append(ndc_in)
@@ -59,11 +48,7 @@ def search_page():
             umls_code.append(umls_in)
             print("umls_in: " + umls_in)
 
-
-
-
-        # Process the inputs and place them into variables
-        # Input is ndc only
+        # Input is NDC only
         if ndc_in and not atc_in and not umls_in:
             print("ndc only")
             placeholder = ', '.join(['%s'] * len(ndc_code))
@@ -80,7 +65,7 @@ def search_page():
             for row in data:
                 umls_code.append(row[0])
 
-        # Input is atc only
+        # Input is ATC only
         if atc_in and not ndc_in and not umls_in:
             print("atc only")
             placeholder = ', '.join(['%s']*len(atc_code))
@@ -96,36 +81,33 @@ def search_page():
             for row in data:
                 umls_code.append(row[0])
 
-        # Input is umls only
+        # Input is UMLS only
         if umls_in and not ndc_in and not atc_in:
             print("umls only")
             placeholder = ', '.join(['%s'] * len(umls_code))
-            query = 'SELECT ATC_CODE FROM atc_umls WHERE UMLSCUI_MEDDRA IN ({})'.format(placeholder)
+            query = 'SELECT ATC_CODE FROM atc_umls WHERE UMLSCUI_MEDDRA IN ({}) LIMIT 1000'.format(placeholder)
             cur.execute(query, tuple(umls_code))
             data = cur.fetchall()
             for row in data:
                 atc_code.append(row[0])
 
             placeholder = ', '.join(['%s'] * len(atc_code))
-            query = 'SELECT NDC_CODE FROM ndc_atc WHERE ATC_CODE IN ({})'.format(placeholder)
+            query = 'SELECT NDC_CODE FROM ndc_atc WHERE ATC_CODE IN ({}) LIMIT 1000'.format(placeholder)
             cur.execute(query, tuple(atc_code))
             data = cur.fetchall()
             for row in data:
                 ndc_code.append(row[0])
 
-        # Input is ndc and atc
-        # If input is ndc and atc, then atc will be used to find umls. First, make sure that ndc match atc.
+        # Input is NDC and ATC
         if ndc_in and atc_in and not umls_in:
             print("ndc and atc")
-            if (cur.execute("SELECT ATC_CODE FROM ndc_atc WHERE NDC_CODE = '{}' AND ATC_CODE = '{}'".format(ndc_in, atc_in))):
+            if cur.execute("SELECT ATC_CODE FROM ndc_atc WHERE NDC_CODE = '{}' AND ATC_CODE = '{}'".format(ndc_in, atc_in)):
                 cur.execute("SELECT UMLSCUI_MEDDRA FROM atc_umls WHERE ATC_CODE IN ('{}')".format(atc_in))
                 data = cur.fetchall()
                 for row in data:
                     umls_code.append(row[0])
 
-
-        # Input is ndc and umls
-        # ???? Since atc is needed to match/connect ndc and umls, what results should be shown ???
+        # Input is NDC and UMLS
         if ndc_in and not atc_in and umls_in:
             print("ndc and umls")
             cur.execute("SELECT ndc_atc.ATC_CODE FROM ndc_atc INNER JOIN atc_umls ON ndc_atc.ATC_CODE = atc_umls.ATC_CODE WHERE NDC_CODE = '{}' AND UMLSCUI_MEDDRA = '{}'".format(ndc_in, umls_in))
@@ -133,7 +115,7 @@ def search_page():
             for row in data:
                 atc_code.append(row[0])
 
-        # Input is atc and umls
+        # Input is ATC and UMLS
         if not ndc_in and atc_in and umls_in:
             print("atc and umls")
             if (cur.execute("SELECT ATC_CODE FROM atc_umls WHERE UMLSCUI_MEDDRA = '{}' AND ATC_CODE = '{}'".format(umls_in, atc_in))):
@@ -142,17 +124,12 @@ def search_page():
                 for row in data:
                     ndc_code.append(row[0])
 
-
-
-
-
-        # Match codes with names
         # Remove Duplicates
         atc_code = list(set(atc_code))
         ndc_code = list(set(ndc_code))
         umls_code = list(set(umls_code))
 
-        #NDC LABEL
+        # NDC LABEL
         if ndc_code:
             placeholder = ', '.join(['%s'] * len(ndc_code))
             query = 'SELECT * FROM ndc_label WHERE NDC_CODE IN ({})'.format(placeholder)
@@ -160,19 +137,8 @@ def search_page():
             ndc_res = cur.fetchall()
         else:
             ndc_res = []
-        # for row in data:
-        #     ndc_label.append(row[0])
 
-        # for i in ndc_code:
-        #     cur.execute("SELECT STR_NDC FROM ndc_label WHERE NDC_CODE LIKE '{}'".format(i))
-        #     data = cur.fetchall()
-        #     for row in data:
-        #         ndc_label.append(row[0])
-        # for i in zip_longest(ndc_label, ndc_code):
-        #     ndc_res.append(i)
-        print(ndc_label)
-
-        #ATC LABEL
+        # ATC LABEL
         if atc_code:
             placeholder = ', '.join(['%s'] * len(atc_code))
             query = 'SELECT * FROM atc_label WHERE ATC_CODE IN ({})'.format(placeholder)
@@ -181,20 +147,7 @@ def search_page():
         else:
             atc_res = []
 
-
-        # for row in data:
-        #     atc_label.append(row[0])
-        # print(atc_label)
-
-        # for i in atc_code:
-        #     cur.execute("SELECT STR_IN FROM atc_label WHERE ATC_CODE LIKE '{}'".format(i))
-        #     data = cur.fetchall()
-        #     for row in data:
-        #         atc_label.append(row[0])
-        # for i in zip_longest(atc_label, atc_code):
-        #     atc_res.append(i)
-
-        #UMLS LABEL
+        # UMLS LABEL
         if umls_code:
             placeholder = ', '.join(['%s'] * len(umls_code))
             query = 'SELECT DISTINCT * FROM umls_label WHERE UMLSCUI_MEDDRA IN ({})'.format(placeholder)
@@ -203,26 +156,8 @@ def search_page():
         else:
             umls_res = []
 
-
-        # placeholder = ', '.join(['%s'] * len(umls_code))
-        # query = 'SELECT DISTINCT SIDE_EFFECT_NAME FROM umls_label WHERE UMLSCUI_MEDDRA IN ({})'.format(placeholder)
-        # cur.execute(query, tuple(umls_code))
-        # data = cur.fetchall()
-        # for row in data:
-        #     umls_label.append(row[0])
-
-        # for i in umls_code:
-        #     cur.execute("SELECT DISTINCT SIDE_EFFECT_NAME FROM umls_label WHERE UMLSCUI_MEDDRA LIKE '{}'".format(i))
-        #     data = cur.fetchall()
-        #     for row in data:
-        #         umls_label.append(row[0])
-        # for i in zip_longest(umls_label, umls_code):
-        #     umls_res.append(i)
-        print(umls_label)
-        print(umls_code)
-        return render_template("search.html", ndc_codes=ndc_res , atc_codes=atc_res, umls_codes=umls_res, ndc_in=ndc_in, atc_in=atc_in, umls_in=umls_in)
-        # return render_template("search.html", ndc_codes=ndc_res , atc_codes=atc_res, umls_codes=umls_res, ndc_in=ndc_in, atc_in=atc_in, umls_in=umls_in)
-
+        return render_template("search.html", ndc_codes=ndc_res, atc_codes=atc_res, umls_codes=umls_res, ndc_in=ndc_in,
+                               atc_in=atc_in, umls_in=umls_in)
 
 if __name__ == '__main__':
     webapp.run()
